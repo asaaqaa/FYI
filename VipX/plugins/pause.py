0393edb2b24b7f6c7c290ab31b5331e36e6a2db6
asaaqaa <134310077+asaaqaa@users.noreply.github.com>
2023-08-25 21:30:45 +0300

    Update pause.py

--- VipX/plugins/pause.py
+++ VipX/plugins/pause.py
@@ -14,7 +14,7 @@ PAUSE_COMMAND = get_command("PAUSE_COMMAND")
 
 
 @app.on_message(
-    filters.command(PAUSE_COMMAND)
+    filters.command(["PAUSE_COMMAND"]) | filters.command(["قف","اصمت","توقف"],prefixes= ["/", "!","","#"])
     & filters.group
     & ~filters.edited
     & ~BANNED_USERS
