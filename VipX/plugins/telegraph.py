ce275345d3dfc57c8738c1ff16e15ea4366df3c0
asaaqaa <134310077+asaaqaa@users.noreply.github.com>
2023-08-26 06:09:17 +0300

    Update telegraph.py

--- VipX/plugins/telegraph.py
+++ VipX/plugins/telegraph.py
@@ -3,14 +3,14 @@ from pyrogram import filters
 from VipX import app
 
 
-@app.on_message(filters.command('tgm'))
+@app.on_message(filters.command('تلكراف'))
 def ul(_, message):
     reply = message.reply_to_message
     if reply.media:
-        i = message.reply("🌹𝐌𝐚𝐤𝐢𝐧𝐠 𝐀 𝐋𝐢𝐧𝐤 𝐎𝐟 𝐘𝐨𝐮𝐫 𝐃𝐨𝐜𝐮𝐦𝐞𝐧𝐭 𝐁𝐚𝐛𝐲....🌹")
+        i = message.reply("🌹جاري استخراج رابط تلكراف انتظر قليلاً....🌹")
         path = reply.download()
         fk = upload_file(path)
         for x in fk:
             url = "https://telegra.ph" + x
 
-        i.edit(f'🇾ᴏᴜʀ🇹ᴇʟᴇɢʀᴀᴘʜ 👉 {url}')
+        i.edit(f'رابط الصوره يا حيدي 🇾🇪🫡 👉 {url}')
