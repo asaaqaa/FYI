cbbab5a723d6fa7988a5935498ff2e070364b80f
asaaqaa <134310077+asaaqaa@users.noreply.github.com>
2023-08-26 05:22:23 +0300

    Update settings.py

--- VipX/utils/inline/settings.py
+++ VipX/utils/inline/settings.py
@@ -8,23 +8,23 @@ def setting_markup(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text="ᴀᴜᴅɪᴏ ǫᴜᴀʟɪᴛʏ", callback_data="AQ"
+                text="✦جودة الصوت✦", callback_data="AQ"
             ),
             InlineKeyboardButton(
-                text="ᴠɪᴅᴇᴏ ǫᴜᴀʟɪᴛʏ", callback_data="VQ"
-            ),
-        ],
-        [
-            InlineKeyboardButton(
-                text="ᴀᴜᴛʜ ᴜsᴇʀs", callback_data="AU"
-            ),
-            InlineKeyboardButton(
-                text="ᴄʟᴇᴀɴ ᴍᴏᴅᴇ", callback_data="CM"
+                text="✦جودة الفيديو✦", callback_data="VQ"
             ),
         ],
         [
             InlineKeyboardButton(
-                text="• sᴜᴩᴩᴏʀᴛ •", url=f"{SUPPORT_GROUP}",
+                text="✦المسموح لهم✦", callback_data="AU"
+            ),
+            InlineKeyboardButton(
+                text="✦الوضع النضيف✦", callback_data="CM"
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="• الدعم •", url=f"{SUPPORT_GROUP}",
             ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"], callback_data="close"
